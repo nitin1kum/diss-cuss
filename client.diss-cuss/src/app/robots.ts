475754e3f17968f29c63d310e54f6ef2aa7d2bab
nitin1kum <mk3529895@gmail.com>
2025-06-02 15:31:38 +0530

    updated robots.ts

--- client.diss-cuss/src/app/robots.ts
+++ client.diss-cuss/src/app/robots.ts
@@ -6,7 +6,6 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: '*',
       allow: '/',
-      disallow: ['/auth','/api'],
     },
     sitemap: `${baseUrl}/sitemap_index.xml`,
   }
