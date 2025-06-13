5599609ab9f0147ec075af584543b738658c4943
nitin1kum <mk3529895@gmail.com>
2025-06-13 14:30:01 +0530

    change cors config

--- server.diss-cuss/src/config/cors-config.ts
+++ server.diss-cuss/src/config/cors-config.ts
@@ -1,6 +1,6 @@
 import { CorsOptions } from "cors";
 
-const whitelist = ["http://localhost:3000",`${process.env.NEXT_BASE_URL}`];
+const whitelist = (process.env.ALLOWED_ORIGINS)?.split(",") || ["http://localhost:3000",process.env.NEXT_BASE_URL!];
 
 export const corsOptions : CorsOptions = {
   origin : whitelist,
